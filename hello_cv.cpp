f3bf8bd3af1cdff6409126828c8124521d4b376c
kiuchiryota <kiuchiryota@team-lab.com>
2023-04-27 13:51:21 +0900

    update : 単純なThreshによる二極化で作ったMaskで映り込みノイズを消す仕組みを実装

--- hello_cv.cpp
+++ hello_cv.cpp
@@ -1,26 +1,21 @@
 #include <opencv2/opencv.hpp>
 using namespace cv;
 
-double FindBestMatchRect(Mat &targetImage, Mat &templateImage, Rect &outRect, Mat *mask = NULL)
+double FindBestMatchRect(Mat &fullImage, Mat &templateImage, Rect &outRect)
 {
-
     Mat result;
-    matchTemplate(targetImage, templateImage, result, TM_CCOEFF_NORMED);
-    outRect.height = targetImage.cols;
-    outRect.width = targetImage.rows;
+
+    matchTemplate(fullImage, templateImage, result, TM_CCOEFF_NORMED);
+
+    outRect.height = templateImage.cols;
+    outRect.width = templateImage.rows;
     Point maxPt;
     double maxValue;
-    if (mask == NULL)
-    {
-        minMaxLoc(result, NULL, &maxValue, NULL, &maxPt);
-    }
-    else
-    {
-        minMaxLoc(result, NULL, &maxValue, NULL, &maxPt, *mask);
-    }
+
+    minMaxLoc(result, NULL, &maxValue, NULL, &maxPt);
 
     std::cout << "(" << maxPt.x << "," << maxPt.y << ")"
-              << "score:" << maxValue;
+              << "score:" << maxValue<<"\n";
 
     outRect.x = maxPt.x;
     outRect.y = maxPt.y;
@@ -28,30 +23,65 @@ double FindBestMatchRect(Mat &targetImage, Mat &templateImage, Rect &outRect, Ma
     return maxValue;
 }
 
+double FindBestMatchRect(Mat &fullImage, Mat &templateImage, Rect &outRect, Mat mask)
+{
+
+    Mat result;
+
+    matchTemplate(fullImage, templateImage, result, TM_CCOEFF_NORMED, mask);
+    outRect.height = templateImage.cols;
+    outRect.width = templateImage.rows;
+    Point maxPt;
+    double maxValue;
+
+    minMaxLoc(result, NULL, &maxValue, NULL, &maxPt);
+
+    std::cout << "(" << maxPt.x << "," << maxPt.y << ")"
+              << "score:" << maxValue << "\n";
+
+    outRect.x = maxPt.x;
+    outRect.y = maxPt.y;
+
+    return maxValue;
+}
+
+void SampleAlphaMask(Mat &inImage, Mat &outMask)
+{
+    int width = inImage.rows;
+    int height = inImage.cols;
+    Mat gray;
+    Mat binary;
+
+    cvtColor(inImage, gray, COLOR_BGR2GRAY);
+    // カメラによってダイナミックレンジが違いそうなので、閾値を自動で設定するadaptiveSamplingを採用
+    //adaptiveThreshold(gray, outMask, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 11, 2);
+    // adaptiveThreshold(gray, outMask, 255, CALIB_CB_ADAPTIVE_THRESH, THRESH_BINARY, 11, 2);
+    cv::threshold(gray,outMask,100,255,THRESH_BINARY);
+}
+
 int main()
 {
     const double MATCH_THRESHOLD = 0.8;
-    const double BINARIZATION_THRESHOLD = 240;
     String srcPath = "noize1.jpeg";
     /*
     std::cout << "src?";
     std::cin >> srcPath;
     */
-    Mat partImage = imread(srcPath);
-    Mat templateImage = imread("suzsiki.jpeg");
+    Mat templateImage = imread(srcPath);
+    Mat fullImage = imread("suzsiki.jpeg");
     Rect outRect;
-    if (FindBestMatchRect(partImage, templateImage, outRect) < MATCH_THRESHOLD)
+    if (FindBestMatchRect(fullImage, templateImage, outRect) < MATCH_THRESHOLD)
     {
+        Mat mask;
         // ここを動的に検出できるようにする
-        Mat mask = imread("mask.png");
-        imshow("mask",mask);
+        SampleAlphaMask(templateImage, mask);
 
-        //int max = FindBestMatchRect(partImage, templateImage, outRect, &mask);
-        //std::cout << "max:" << max;
+        //imshow("mask", mask);
+        int max = FindBestMatchRect(fullImage, templateImage, outRect, mask);
     }
 
-    rectangle(templateImage, outRect, Scalar(0, 255, 255), 3);
+    rectangle(fullImage, outRect, Scalar(0, 255, 255), 3);
 
-    imshow("result", templateImage);
+    imshow("result", fullImage);
     waitKey();
 }
